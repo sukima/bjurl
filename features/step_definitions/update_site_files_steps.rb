d5f89ce8fb7865b759a718659e5b38d3616f6c2e
Devin Weaver <suki@tritarget.org>
2011-12-14 16:35:20 -0500

    Fixes bad path for perl
    Also adds gitignore to ignore garbage left by aruba (tmp directory)

--- features/step_definitions/update_site_files_steps.rb
+++ features/step_definitions/update_site_files_steps.rb
@@ -15,5 +15,6 @@ Given /^someone has posted "([^"]*)"$/ do |arg1|
 end
 
 When /^the html files are updated$/ do
-    system "perl tester.pl update_site_files -- #{@line} -- #{@settings}"
+    # aruba's sandbox default is "tmp/aruba" therefore all files needed are found in "../.."
+    run_simple(unescape("perl -I../.. ../../tester.pl update_site_files -- #{@line} -- #{@settings}"))
 end
