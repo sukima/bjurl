3ccd9ba5b8edf557fbdbafc49a0a2a7ec7c17916
Devin Weaver <suki@tritarget.org>
2011-12-15 20:35:17 -0500

    Adds initial structure for qunit test
    Includes tests for show_error

--- test/tests.js
+++ test/tests.js
@@ -1,34 +1,61 @@
+// Helper function buildJSONObject() {{{
+var buildJSONObject = function(text) {
+    var a;
+    var json = '[';
+    if (typeof text == "array") {
+        a = text;
+    } else {
+        a = [ text ];
+    }
+    for (var i=0; i < a.length; i++) {
+        json = json + '{"time":"timestamp","nick":"nickname","message":"' + a[i] + '"}';
+        if (i<a.length-1) { json = json + ','; }
+    }
+    json = json + ']';
+    return json;
+}; // }}}
+
+
 $(document).ready(function(){
     if (Site) {
         // Check that the main script is loaded
         // If so we no longer need the warning
         $("#testwarning").remove();
     }
-    /*
-Site.show_error = function() {
-test("a basic test example", function() {
-  ok( true, "this test is fine" );
-  var value = "hello";
-  equal( value, "hello", "We expect value to be hello" );
+
+
+    // Module show_error {{{1
+    module("show_error", {
+        setup: function() {
+            this.err = $("<div/>",{id:"error"}).appendTo("#qunit-fixture");
+        }
+    });
+    test("hides #error when error_msg is empty", function() {
+        Site.error_msg = "";
+        Site.show_error();
+        ok(!this.err.is(":visible"), "#error is not visible");
+    });
+    test("Displays #error when error_msg is not empty", function() {
+        Site.error_msg = "error_msg_test_string";
+        Site.show_error();
+        ok(this.err.is(":visible"), "#error is visible");
+        equal(this.err.text(), "error_msg_test_string", "#error equals 'error_msg_test_string'");
+        equal(Site.error_msg, "", "Site.error_msg was reset");
+    });
+
+
+    // Module populate {{{1
+    module("populate", {
+        setup: function() {
+            this.update_time = $("<div/>",{id:"update-time"}).appendTo("#qunit-fixture");
+            this.url_list = $("<div/>",{id:"url-list"}).appendTo("#qunit-fixture");
+        }
+    });
+    test("", function() {
+        ok(false, "pending");
+    });
+
+
+// }}}1
 });
-
-module("Module A");
-
-test("first test within module", function() {
-  ok( true, "all pass" );
-});
-
-test("second test within module", function() {
-  ok( true, "all pass" );
-});
-
-module("Module B");
-
-test("some other test", function() {
-  expect(2);
-  equal( true, false, "failing test" );
-  equal( true, true, "passing test" );
-});
-*/
-
-});
+/* vim:set fdm=marker: */
