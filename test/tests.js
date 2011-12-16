0a4dd70a7baf5168cb3f9eb85fbdb034c4182d97
Devin Weaver <suki@tritarget.org>
2011-12-15 19:29:45 -0500

    Adds scafold for Qunit tests

---
+++ test/tests.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    if (Site) {
+        // Check that the main script is loaded
+        // If so we no longer need the warning
+        $("#testwarning").remove();
+    }
+    /*
+Site.show_error = function() {
+test("a basic test example", function() {
+  ok( true, "this test is fine" );
+  var value = "hello";
+  equal( value, "hello", "We expect value to be hello" );
+});
+
+module("Module A");
+
+test("first test within module", function() {
+  ok( true, "all pass" );
+});
+
+test("second test within module", function() {
+  ok( true, "all pass" );
+});
+
+module("Module B");
+
+test("some other test", function() {
+  expect(2);
+  equal( true, false, "failing test" );
+  equal( true, true, "passing test" );
+});
+*/
+
+});
