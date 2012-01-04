1ba5489c81790b38d4a227ac4c24bf7fa2ce4e61
Devin Weaver <suki@tritarget.org>
2012-01-04 09:25:06 -0500

    Adds vebose output for debugging

--- test/irssi.pm
+++ test/irssi.pm
@@ -7,35 +7,41 @@ $VERSION = "20020121.2020";
 our %script_options = ( );
 
 sub settings_add_str {
+    print "settings_add_str called with @_\n";
     $script_options{ $_[1] } = $_[2]; # ignore first argument
 }
 
 sub settings_add_bool {
+    print "settings_add_bool called with @_\n";
     $script_options{ $_[1] } = ($_[2]) ? "ON" : "OFF"; # ignore first argument
 }
 
-sub signal_add { 1; } # do nothing, stub
+sub signal_add { print "signal_add called with @_\n"; }
 
-sub command_bind { 1; } # do nothing, stub
+sub command_bind { print "command_bind called with @_\n"; }
 
 sub settings_get_str {
+    print "settings_get_str called for $_[0]\n";
     return $script_options{ $_[0] };
 }
 
 sub settings_get_bool {
+    print "settings_get_bool called for $_[0]\n";
     return ($script_options{ $_[0] } =~ /ON/i) ? 1 : 0;
 }
 
 sub level2bits {
+    print "level2bits called\n";
     return 0;
 }
 
-sub signal_emit { 1; } # do nothing, stub
-
-sub signal_stop { 1; } # do nothing, stub
-
-sub print {
-    print "@_\n";
+sub signal_emit {
+    print "signal_emit called:\n";
+    print "  @_\n";
 }
+
+sub signal_stop { print "signal_stop called\n"; }
+
+sub print { print "@_\n"; }
 
 1;
