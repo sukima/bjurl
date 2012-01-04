f04ffb3d69ce2be90b28c18c15414f4f6a856918
Devin Weaver <suki@tritarget.org>
2012-01-04 11:49:44 -0500

    Fixes garbage left in temp directory

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -6,9 +6,19 @@ OLDDIR=`pwd`
 DIR=`dirname $0`
 cd $DIR/..
 DIR=`pwd` # Make path absolute
+
+# Run cucumber tests
 cucumber
-perl -I. -Itest test/tester.pl update_site_files -- this is a test http://foobar.com/ -- url_use_webapp=ON url_html_location=tmp/aruba/test
+
+# Clean temp files
+rm -f tmp/aruba/test/*
+
+# Rebuild web files
+perl -I. -Itest test/tester.pl update_site_files -- this is a test http://foobar.com/ -- url_use_webapp=ON url_html_location=tmp/aruba/test >&-
+
+# return to previous directory
 cd $OLDDIR
+
 echo
 echo "To test JavaScript please open your browser to file://$DIR/test/test.html"
 echo
