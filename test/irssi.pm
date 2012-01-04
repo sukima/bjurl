7bc8c191dc7eec6ce37172e89744944e5954e56d
Devin Weaver <suki@tritarget.org>
2012-01-04 15:48:20 -0500

    Adds Data::Dumper output

--- test/irssi.pm
+++ test/irssi.pm
@@ -1,13 +1,14 @@
 # Setup mocks for the Irssi specific code.
 
 package Irssi;
+use Data::Dumper;
 
 $VERSION = "20020121.2020";
 
 our %script_options = ( );
 
 sub settings_add_str {
-    print "settings_add_str called with @_\n";
+    print "settings_add_str called with ". Dumper(@_) ."\n";
     $script_options{ $_[1] } = $_[2]; # ignore first argument
 }
 
@@ -16,17 +17,17 @@ sub settings_add_bool {
     $script_options{ $_[1] } = ($_[2]) ? "ON" : "OFF"; # ignore first argument
 }
 
-sub signal_add { print "signal_add called with @_\n"; }
+sub signal_add { print "signal_add called with ". Dumper(@_) ."\n"; }
 
-sub command_bind { print "command_bind called with @_\n"; }
+sub command_bind { print "command_bind called with ". Dumper(@_) ."\n"; }
 
 sub settings_get_str {
-    print "settings_get_str called for $_[0]\n";
+    print "settings_get_str called for ". Dumper($_[0]) ."\n";
     return $script_options{ $_[0] };
 }
 
 sub settings_get_bool {
-    print "settings_get_bool called for $_[0]\n";
+    print "settings_get_bool called for ". Dumper($_[0]) ."\n";
     return ($script_options{ $_[0] } =~ /ON/i) ? 1 : 0;
 }
 
@@ -36,12 +37,12 @@ sub level2bits {
 }
 
 sub signal_emit {
-    print "signal_emit called:\n";
-    print "  @_\n";
+    print "signal_emit called: ";
+    print  Dumper(@_) ."\n";
 }
 
 sub signal_stop { print "signal_stop called\n"; }
 
-sub print { print "@_\n"; }
+sub print { print Dumper(@_); }
 
 1;
